c637b9f8bec45a1c6f87557d00ea9f9b02fe30f4
Nugroho Adi Pramono <nugnux@gmail.com>
2024-09-09 10:13:57 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -9,4 +9,17 @@ if st.button("Say hello"):
     st.write("Why hello there")
 else:
     st.write("Goodbye")
+color = st.select_slider(
+    "Select a color of the rainbow",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+)
+st.write("My favorite color is", color)
 
