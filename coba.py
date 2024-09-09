5513c2a41bebe6af0891dd017160a30f6ba333f9
Nugroho Adi Pramono <nugnux@gmail.com>
2024-09-09 10:12:42 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -4,4 +4,9 @@ st.title('Fisika')
 st.header('Komputasi Awan')
 st.text('Tulisan')
 print('Tadaa...')
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
 
