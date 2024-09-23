0264bf31764c4d1cb56c0e274067e53817cc6a75
Nugroho Adi Pramono <nugnux@gmail.com>
2024-09-23 09:48:28 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 x = st.number_input("Masukkan angka")
-st.write("Anda memasukkan angka", x)
-sx = st.text_input("Movie title", "Life of Brian")
-st.write (sx)
+sx = st.text_input("Satuan", "C")
+st.write ("Anda memasukkan", x,' ',sx)
 
