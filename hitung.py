220ffa56c59ac5338dbe5f78a77ef2e36ccf9d08
Nugroho Adi Pramono <nugnux@gmail.com>
2024-09-23 10:40:31 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 x = st.number_input("Masukkan angka")
-sx = st.selectbox("Satuan", ("C", "F", "R","K"), key=sx)
+sx = st.selectbox("Satuan", ("C", "F", "R","K"), key='sx')
 st.write ("Anda memasukkan", x,' ',sx)
-sy = st.selectbox("Dikonversi ke", ("C", "F", "R","K"), key=sy)
+sy = st.selectbox("Dikonversi ke", ("C", "F", "R","K"), key='sy')
 y = 0
 if (sx == 'C'):
   if(sy == 'C'):
