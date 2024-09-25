c0f8eb10aa4585ec4e4fbbbe5953d3f1f55bdfdb
Nugroho Adi Pramono <nugnux@gmail.com>
2024-09-25 09:52:38 +0700

    Update input.py

--- input.py
+++ input.py
@@ -4,5 +4,9 @@ x = st.number_input("Masukkan suhu")
 sx = st.text_input("Satuan", "C")
 st.write("Anda menginput suhu ", x," dengan satuan ", sx )
 sy = st.text_input("konversi ke", "C")
+if (sx == "C"):
+  pass
+else:
+  pass
 
 st.write("Hasil konversi dari ",x,sx," adalah ...", sy )
