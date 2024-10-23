735ad529145c7645f03aa2a2f3688c0d0c3cb275
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-23 20:04:52 +0700

    Update acak.py

--- acak.py
+++ acak.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import random
+import matplotlib.pyplot as plt
 
 
 st.title("Fisika Komputasi Awan")
@@ -9,3 +10,5 @@ y = []
 if st.button("Data"):
     x.append(random.random() - .5)
     y.append(random.random() - .5)
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(x, y, label='cos(x)', color='g') 
