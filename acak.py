5e6ae0f177edc794dba903371c762616c4951f03
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-23 20:34:12 +0700

    Update acak.py

--- acak.py
+++ acak.py
@@ -7,19 +7,24 @@ st.title("Fisika Komputasi Awan")
 st.title("Nugroho Adi Pramono :sunglasses:")
 x = []
 y = []
+color = []
+size = []
 x.append(0)
 y.append(0)
 if st.button("Data"):
     for i in range(10):
         x.append(random.random() - .5)
         y.append(random.random() - .5)
+        c.append(random.random() )
+        s.append(random.random() )
     
 fig, ax = plt.subplots(figsize=(16, 8))
-ax.plot(x, y, 'go',label='Plot titik acak') 
+ax.plot(x, y, c=color, s=size, alpha=0.5) 
 
-ax.set_ylabel("")
+ax.set_ylabel("y")
 ax.set_xlabel("x")
 ax.tick_params(axis='y', labelsize=20)
 ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
 ax.tick_params(axis='x', labelsize=15)
+ax.set_title('Data Acak yang berubah setiap tombol ditekan')
 st.pyplot(fig)
