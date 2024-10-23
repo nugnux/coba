b3c2efd5c7a9a491682054bf9e3962508b20499e
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-23 20:17:04 +0700

    Update acak.py

--- acak.py
+++ acak.py
@@ -7,8 +7,18 @@ st.title("Fisika Komputasi Awan")
 st.title("Nugroho Adi Pramono :sunglasses:")
 x = []
 y = []
+x.append(0)
+y.append(0)
 if st.button("Data"):
     x.append(random.random() - .5)
     y.append(random.random() - .5)
+    
 fig, ax = plt.subplots(figsize=(16, 8))
-ax.plot(x, y, label='cos(x)', color='g') 
+ax.plot(x, y, label='Plot titik acak', color='g') 
+
+ax.set_ylabel("")
+ax.set_xlabel("x")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
