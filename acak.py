8100bfbb6b65d954500bf9376379e8cdfe965d0a
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-23 21:37:38 +0700

    Update acak.py

--- acak.py
+++ acak.py
@@ -11,8 +11,8 @@ color = []
 size = []
 x.append(0)
 y.append(0)
-color.append((0.,0.,0.))
-size.append(10)
+color.append((0.,.7,0.))
+size.append(73)
 if st.button("Data"):
     for i in range(10):
         x0 = random.random() - .5
@@ -22,9 +22,9 @@ if st.button("Data"):
             y0 = y0/2.
             
         x.append(x0)
-        y.append(y0)
+        y.append(y0
         color.append((random.random(),random.random(),random.random()))
-        size.append(713*random.random() )
+        size.append(1713*random.random() )
     
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.scatter(x, y, c=color, s=size, alpha=0.5) 
@@ -35,4 +35,6 @@ ax.tick_params(axis='y', labelsize=20)
 ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
 ax.tick_params(axis='x', labelsize=15)
 ax.set_title('Data Acak yang berubah setiap tombol ditekan')
+ax.set_xlim([-1, 1])
+ax.set_ylim([-1, 1])
 st.pyplot(fig)
