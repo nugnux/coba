d5ef8d486e66fe9c69ab9e2a18e0f418a5a8c73b
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-23 19:45:25 +0700

    Create acak.py

---
+++ acak.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Fisika Komputasi Awan")
+st.title("Nugroho Adi Pramono :sunglasses:")
+
+st.button("Data", type="primary")
+if st.button("Data"):
+    st.write("Why hello there")
