cee470ed63a2dd69cd7365f8620cf888b31057c4
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-23 19:47:31 +0700

    Update acak.py

--- acak.py
+++ acak.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.title("Fisika Komputasi Awan")
 st.title("Nugroho Adi Pramono :sunglasses:")
 
-st.button("Data", type="primary")
 if st.button("Data"):
     st.write("Why hello there")
